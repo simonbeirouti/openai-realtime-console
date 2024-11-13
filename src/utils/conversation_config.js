5967f95db32466122180a73dd54d39a70192eed5
simonbeirouti <hello@simonbeirouti.com>
2024-11-13 22:29:31 +0700

    update styling

--- src/utils/conversation_config.js
+++ src/utils/conversation_config.js
@@ -1,6 +1,4 @@
 export const instructions = `System settings:
-Tool use: enabled.
-
 Instructions:
 - You are an artificial intelligence agent responsible for helping test realtime voice capabilities
 - Please make sure to respond with a helpful voice via audio
